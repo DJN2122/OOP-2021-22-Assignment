c005a8ac9272a56d4f00aee9be3b0e845ca88501
Krys24 <kpakos2002@gmail.com>
2022-04-11 13:08:39 +0100

    Cleaned up the code

--- java/src/ie/tudublin/Audio.java
+++ java/src/ie/tudublin/Audio.java
@@ -1,11 +1,13 @@
 package ie.tudublin;
 
+// import all relevant packages
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
 
+// audio class
 public class Audio extends PApplet
 {
     Minim minim;
@@ -20,31 +22,40 @@ public class Audio extends PApplet
     float smoothedY = 0;
     float smoothedAmplitude = 0;
 
-    public void keyPressed() {
-		if (key >= '0' && key <= '9') {
+    public void keyPressed() 
+    {
+		if (key >= '0' && key <= '9')
+        {
 			mode = key - '0';
 		}
 
-		if (keyCode == ' ') {
-            if (ap.isPlaying()) {
+		if (keyCode == ' ') 
+        {
+            if (ap.isPlaying()) 
+            {
                 ap.pause();
-            } else {
+            } 
+
+            else 
+            {
                 ap.rewind();
                 ap.play();
             }
         }
         
-        if (keyCode == ESC) {
-            exit();
+        if (keyCode == ESC) 
+        {
+            exit(); // exits out of program when "ESC" is pressed
         }
 	}
 
+    // settings method
     public void settings()
     {
-        //size(1024, 1000, P3D);
         fullScreen(P3D, SPAN); //P3D
     }
 
+    // setup method
     public void setup()
     {
         minim = new Minim(this);
@@ -64,9 +75,9 @@ public class Audio extends PApplet
 
     float off = 0;
 
+    // draw method
     public void draw()
     {
-        //background(0);
         float halfH = height / 2;
         float average = 0;
         float sum = 0;
@@ -85,8 +96,11 @@ public class Audio extends PApplet
         float cx = width / 2;
         float cy = height / 2;
 
-        switch (mode) {
+        switch (mode) 
+        {
+            // case when "0" is pressed
 			case 0:
+            {
                 background(0);
                 for(int i = 0 ; i < ab.size() ; i ++)
                 {
@@ -97,75 +111,92 @@ public class Audio extends PApplet
                     line(i, halfH + f, i, halfH - f);                    
                 }
                 break;
+            }
 
-        case 1:
-            background(0);
-            for(int i = 0 ; i < ab.size() ; i ++)
+            // case when "1" is pressed
+            case 1:
             {
-                //float c = map(ab.get(i), -1, 1, 0, 255);
-                float c = map(i, 0, ab.size(), 0, 255);
+                background(0);
+                for(int i = 0 ; i < ab.size() ; i ++)
+                {
+                    //float c = map(ab.get(i), -1, 1, 0, 255);
+                    float c = map(i, 0, ab.size(), 0, 255);
+                    stroke(c, 255, 255);
+                    float f = lerpedBuffer[i] * halfH * 4.0f;
+                    line(i, halfH + f, halfH - f, i);                    
+                }
+                break;
+            }
+
+            // case when "2" is pressed
+            case 2:
+            {
+                background(0, 0, 0, 10);
+                float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
+                stroke(c, 255, 255);	
+                float radius = map(smoothedAmplitude, 0, 0.1f, 50, 300);		
+                int points = (int)map(mouseX, 0, 255, 3, 10);
+                int sides = points * 2;
+                float px = cx;
+                float py = cy - radius; 
+
+                for(int i = 0 ; i <= sides ; i ++)
+                {
+                    float r = (i % 2 == 0) ? radius : radius / 2; 
+                    // float r = radius;
+                    float theta = map(i, 0, sides, 0, TWO_PI);
+                    float x = cx + sin(theta) * r;
+                    float y = cy - cos(theta) * r;
+                            
+                    //circle(x, y, 20);
+                    line(px, py, x, y);
+                    px = x;
+                    py = y;
+                }
+                break;
+            }
+
+            // case when "3" is pressed
+            case 3:
+            {
+                background(0);
+                strokeWeight(2);
+                noFill();
+
+                float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
+                float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
                 stroke(c, 255, 255);
-                float f = lerpedBuffer[i] * halfH * 4.0f;
-                line(i, halfH + f, halfH - f, i);                    
-            }
-            break;
+                circle(cx, cy, r);
 
-        case 2:
+                break;
+            }
+
+            // case when "4" is pressed
+            case 4:
             {
-                    float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-                    background(0, 0, 0, 10);
-                    stroke(c, 255, 255);	
-                    float radius = map(smoothedAmplitude, 0, 0.1f, 50, 300);		
-                    int points = (int)map(mouseX, 0, 255, 3, 10);
-                    int sides = points * 2;
-                    float px = cx;
-                    float py = cy - radius; 
-                    for(int i = 0 ; i <= sides ; i ++)
-                    {
-                        float r = (i % 2 == 0) ? radius : radius / 2; 
-                        // float r = radius;
-                        float theta = map(i, 0, sides, 0, TWO_PI);
-                        float x = cx + sin(theta) * r;
-                        float y = cy - cos(theta) * r;
-                        
-                        //circle(x, y, 20);
-                        line(px, py, x, y);
-                        px = x;
-                        py = y;
-                    }
-                    break;
+                background(0);
+                strokeWeight(2);
+                
+                for(int i = 0 ; i < ab.size() ; i +=10)
+                {
+                    //float c = map(ab.get(i), -1, 1, 0, 255);
+                    float cc = map(i, 0, ab.size(), 0, 255);
+                    stroke(cc, 255, 255);
+                    float f = map(smoothedAmplitude, 0, 0.5f, 0, 255);
+                    //line(i, halfH + f, i, halfH - f);
+                    fill(cc);
+                    circle(i, halfH + f, 5);                    
+                    circle(i, halfH - f, 5);                    
+                }
+                break;
             }
 
-        case 3:
-            background(0);
-            strokeWeight(2);
-            noFill();
-            float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-            float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-            stroke(c, 255, 255);
-            circle(cx, cy, r);
-            break;
-
-        case 4:
-            background(0);
-            strokeWeight(2);
-            for(int i = 0 ; i < ab.size() ; i +=10)
-            {
-                //float c = map(ab.get(i), -1, 1, 0, 255);
-                float cc = map(i, 0, ab.size(), 0, 255);
-                stroke(cc, 255, 255);
-                float f = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-                //line(i, halfH + f, i, halfH - f);
-                fill(cc);
-                circle(i, halfH + f, 5);                    
-                circle(i, halfH - f, 5);                    
-            }
-            break;
-
+            // case when "5" is pressed
             case 5:
             {
                 background(0);
                 strokeWeight(2);
+
                 double i=0;float k=0;
                 float x;
                 float y;
@@ -178,67 +209,84 @@ public class Audio extends PApplet
                 
                 break;
             }
-            case 6:
-            noStroke();
-            fill(0, 5);
-            rect(0,0,width,height);
-            pushMatrix();
-            translate(width/2, height/2);
-            rotate(radians(frameCount % 360 * 2));
-            //float c = map(i, 0, ab.size(), 0, 255);
-            for(int j = 0; j < 360; j++) {
-                
-                if(ap.mix.get(j)*200 > 50) {
-                    stroke(20,255,255);
-                }
-                else {
-                    stroke(100,255,255);
-                }
-                
-                line(cos(j)*50, sin(j)*50, cos(j)*abs(ap.left.get(j))*200 + cos(j)*50, sin(j)*abs(ap.right.get(j))*200 + sin(j)*50);
-            }
-            for(int k = 360; k > 0; k--) {
-                
-                
-                if(ap.mix.get(k)*200 > 25) {
-                stroke(150,255,255);
-            }
-                else {
-                    stroke(200,255,255);
-                }
-                
-                line(cos(k)*50, sin(k)*50, cos(k)*abs(ap.right.get(k))*200 + cos(k)*50, sin(k)*abs(ap.left.get(k))*200 + sin(k)*50);   
-            }
-            
-            popMatrix();
-            break;
-               
-        case 7:
-            {
-                background(0);
-                strokeWeight(2);
-                
-                for(int i = 0 ; i < ab.size() ; i += 10)
-                {
-                    float f = map(smoothedAmplitude, 0, 0.5f, 0, 750);
-                    float j = lerpedBuffer[i] * halfH / 4.0f;
-                    float cc = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-                    stroke(cc, 255, 255);
-                    noFill();
-                    rectMode(CENTER);
-                    float sqSize = f;
-                    float halfSize = f/2;
 
-                    square(cx, cy, sqSize + j);
-                    line(cx + halfSize, cy + halfSize, width, height + j * 2);
-                    line(cx + halfSize, cy - halfSize, width, 0 - j * 2);
-                    line(cx - halfSize, cy - halfSize, 0, 0 - j * 2);
-                    line(cx - halfSize, cy + halfSize, 0, height + j * 2);
-                    // line(i, halfH + f, i, halfH - f); 
-        
+            // case when "6" is pressed
+            case 6:
+            {
+                noStroke();
+                fill(0, 5);
+                rect(0,0,width,height);
+                pushMatrix();
+                translate(width/2, height/2);
+                rotate(radians(frameCount % 360 * 2));
+                //float c = map(i, 0, ab.size(), 0, 255);
+                for(int j = 0; j < 360; j++) 
+                {
+                    
+                    if(ap.mix.get(j)*200 > 50) 
+                    {
+                        stroke(20,255,255);
+                    }
+
+                    else 
+                    {
+                        stroke(100,255,255);
+                    }
+                    
+                    line(cos(j)*50, sin(j)*50, cos(j)*abs(ap.left.get(j))*200 + cos(j)*50, sin(j)*abs(ap.right.get(j))*200 + sin(j)*50);
                 }
+
+                for(int k = 360; k > 0; k--) 
+                {
+                     
+                    if(ap.mix.get(k)*200 > 25) 
+                    {
+                        stroke(150,255,255);
+                    }  
+
+                    else 
+                    {
+                        stroke(200,255,255);
+                    }
+                    
+                    line(cos(k)*50, sin(k)*50, cos(k)*abs(ap.right.get(k))*200 + cos(k)*50, sin(k)*abs(ap.left.get(k))*200 + sin(k)*50);   
+                }
+                
+                popMatrix();
                 break;
             }
+               
+        // case when "7" is pressed
+        case 7:
+        {
+            background(0);
+            strokeWeight(2);
+                
+            for(int i = 0 ; i < ab.size() ; i += 10)
+            {
+                float f = map(smoothedAmplitude, 0, 0.5f, 0, 750);
+                float j = lerpedBuffer[i] * halfH / 4.0f;
+                float cc = map(smoothedAmplitude, 0, 0.5f, 0, 255);
+                stroke(cc, 255, 255);
+                noFill();
+                rectMode(CENTER);
+                float sqSize = f;
+                float halfSize = f/2;
+
+                square(cx, cy, sqSize + j);
+                line(cx + halfSize, cy + halfSize, width, height + j * 2);
+                line(cx + halfSize, cy - halfSize, width, 0 - j * 2);
+                line(cx - halfSize, cy - halfSize, 0, 0 - j * 2);
+                line(cx - halfSize, cy + halfSize, 0, height + j * 2);
+                // line(i, halfH + f, i, halfH - f); 
+        
+            }
+                break;
+        }
+
     }
+
 }
 }
+
+
